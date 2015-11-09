b9a6b02cc259a8d130770e0184f7fe10539915a4
Victor <victormel87@gmail.com>
2015-11-09 12:34:22 +0100

    Creada clase ClockDisplay y constructor

---
+++ ClockDisplay.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class ClockDisplay here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ClockDisplay
+{
+    // instance variables - replace the example below with your own
+    private int horas;
+    private int minutos;
+
+    /**
+     * Creamos un reloj con las horas y minutos pasados como parametros
+     */
+    public ClockDisplay(int hora, int mins)
+    {
+        horas = hora;
+        minutos = mins;
+    }
+
+   
+}
